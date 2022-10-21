f6ac35b7a25052c53bc959cbba07a1082ddde3d3
Pasquale Lisena <pasquale.lisena@eurecom.fr>
2022-10-21 11:46:54 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import csv
+import os
+
+from pyrdf2vec import RDF2VecTransformer
+from pyrdf2vec.embedders import Word2Vec
+from pyrdf2vec.graphs import KG, Vertex
+from pyrdf2vec.walkers import RandomWalker
+from tqdm import tqdm
+
+data_folder = './data'
+
+kg = KG()
+
+print('Reading graph..')
+for x in tqdm(os.listdir(data_folder)):
+    with open(os.path.join(data_folder, x), 'r') as f:
+        csv_reader = csv.reader(f, delimiter=',')
+
+        first = True
+        for s, p, o in csv_reader:
+            if first:
+                first = False
+                continue
+            subj = Vertex(s)
+            obj = Vertex(o)
+            pred = Vertex(p, predicate=True, vprev=subj, vnext=obj)
+            kg.add_walk(subj, pred, obj)
+
+print('Nb of vertices:', len(kg._vertices))
+print('Nb of entities in the graph:', len(kg._entities))
+
+with open(os.path.join('./', 'entities.txt'), 'r') as f:
+    entities = [x.strip() for x in f.readlines()][1:]
+    entities = [x for x in entities if kg.is_exist([x])]
+
+print('Nb of entities for which we are computing embeddings:', len(entities))
+
+transformer = RDF2VecTransformer(
+    Word2Vec(epochs=10),
+    walkers=[RandomWalker(4, 10, with_reverse=False, n_jobs=4)],
+    # verbose=1
+)
+
+print('Generating embeddings...')
+embeddings, literals = transformer.fit_transform(kg, entities)
---
+++ preprocessing.py
@@ -0,0 +1,32 @@
+import os
+from tqdm import tqdm
+from SPARQLWrapper import SPARQLWrapper, CSV
+
+config_folder = './config'
+data_folder = './data'
+
+with open(os.path.join(config_folder, 'prefixes.txt'), 'r') as file:
+    prefixes = [x.strip() for x in file.readlines()]
+
+with open(os.path.join(config_folder, 'object_properties.txt'), 'r') as file:
+    props = [x.strip() for x in file.readlines()]
+
+sparql = SPARQLWrapper("http://data.odeuropa.eu/repositories/odeuropa")
+sparql.setReturnFormat(CSV)
+
+os.makedirs(data_folder, exist_ok=True)
+
+for p in tqdm(props):
+    q = '\n'.join(prefixes) + '\n' + 'SELECT * WHERE { ?s ?p ?o . VALUES ?p {%s}}' % p
+    sparql.setQuery(q)
+    ret = sparql.queryAndConvert()
+    with open(os.path.join(data_folder, p.replace(':', '_') + '.csv'), 'wb') as file:
+        file.write(ret)
+
+with open(os.path.join(config_folder, 'all_vocs_entities.rq'), 'r') as file:
+    query = file.read()
+
+sparql.setQuery(query)
+ret = sparql.queryAndConvert()
+with open(os.path.join('./', 'entities.txt'), 'wb') as file:
+    file.write(ret)
